d482c377738fe81709a390856eeee1be55b69823
EmadEddin <aimad.tchad@gmail.com>
2017-12-21 23:04:04 +0000

    i have creat the slider based on bootstrap ressources, also adding somme fontowsome, some tricks so the slide is stable and responsive also the feature container is stable and responsive

--- js/plugins.js
+++ js/plugins.js
@@ -3,3 +3,16 @@
 	Theme Coder : EmadEddin Had
 	Designer : EmadEddin had
 */
+
+$(function(){
+	// to calculate the hide of slide automaticly
+	var windowHeight = $(window).height(),
+	//innerheght to calculate all the size paddin margin border
+		upperbarHeight = $('.upper-bar').innerHeight(),
+		navbarHeight = $('.navbar').innerHeight();
+		$('.slider .carousel-item').height(windowHeight - (upperbarHeight+navbarHeight));
+
+		$('.carousel').carousel({
+		  interval: 5000
+		});
+});
