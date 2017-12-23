a2b5aef568a6fb75c1e63eb2e1b5205f2d3223eb
EmadEddin <aimad.tchad@gmail.com>
2017-12-23 02:47:38 +0000

    at this point i have finished some effects also the featers services, well everything is stable shuffle script was created by me this time easy to do and better then download a plugins so next steps incha2a lah i will finished all the project by adding the rest of containers and fixing some text size for mobile and also the nav bar

--- js/plugins.js
+++ js/plugins.js
@@ -15,4 +15,51 @@ $(function(){
 		$('.carousel').carousel({
 		  interval: 5000
 		});
+
+	// i made this to fix the probleme of changing from show hide to opacity whene resizing the window
+	$(window).on('resize', function(){
+		$(".feature-service ul li[data-val='all']").addClass('active').siblings().removeClass('active');
+		$('.feature-service .row .col-sm-6').show(1000);
+		$('.feature-service .row .col-sm-6').css('opacity',1);
+	});
+
+	//shuflle features Services
+	$('.feature-service ul li').on('click',function(){
+	    
+	    $(this).addClass('active').siblings().removeClass('active');
+
+	     //i see here that the idea of leting the items opacity minimaze whene choosing one element 
+	    // will not work nicely in mobile phones so it is better to hide element or showing theme
+		if($(window).width()< 570){
+
+			// console.log('.feature-service .row .col-sm-6'+' '+$(this).data('val'));
+			if($(this).data('val') === 'all'){
+				$('.feature-service .row .col-sm-6').show(1000);
+			}
+			else {
+				$('.feature-service .row .col-sm-6').hide(1000);
+				$('.feature-service .row .col-sm-6'+' '+$(this).data('val')).parent().show(1000);
+			}
+
+		}
+
+		else{
+
+			if($(this).data('val') === 'all'){
+				$('.feature-service .row .col-sm-6').css('opacity',1);
+			}
+			else {
+				$('.feature-service .row .col-sm-6').css('opacity',0.5);
+				$('.feature-service .row .col-sm-6'+' '+$(this).data('val')).parent().css('opacity',1);
+			}
+
+		}
+
+
+		
+
+
+	});
+
+
 });
